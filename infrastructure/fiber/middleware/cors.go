a3b3f303fb1e48870df22560a5e5667c54d62822
JunePoon <bukharney@gmail.com>
2024-09-18 17:56:57 +0700

    ci: mod compose

--- infrastructure/fiber/middleware/cors.go
+++ infrastructure/fiber/middleware/cors.go
@@ -1,15 +1,13 @@
 package middleware
 
 import (
-	"strings"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func NewCorsMiddleware(origins []string) fiber.Handler {
 	return cors.New(cors.Config{
-		AllowOrigins:     ""
-		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	})
 }
