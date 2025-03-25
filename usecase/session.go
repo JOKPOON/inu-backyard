a981de8467598474f310bf00050c99fcb05fe8eb
JunePoon <bukharney@gmail.com>
2025-03-25 05:25:57 +0000

    fix; cookie to secue none

--- usecase/session.go
+++ usecase/session.go
@@ -87,7 +87,7 @@ func (u sessionUseCase) Create(
 	cookie := &fiber.Cookie{
 		Name:     u.config.Session.CookieName,
 		SameSite: "None",
-		Domain:   "localhost",
+		Domain:   ".localhost",
 		Path:     "/",
 		Value:    signedId,
 		HTTPOnly: true,
