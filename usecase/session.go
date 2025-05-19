12531b9b0baa8c9a25aec2a78f1677cc7c95ea66
JunePoon <bukharney@gmail.com>
2025-05-19 15:49:42 +0000

    update cookie

--- usecase/session.go
+++ usecase/session.go
@@ -86,7 +86,7 @@ func (u sessionUseCase) Create(
 
 	cookie := &fiber.Cookie{
 		Name:     u.config.Session.CookieName,
-		SameSite: "Strict",
+		SameSite: "Lax",
 		Path:     "/",
 		Value:    signedId,
 		HTTPOnly: true,
